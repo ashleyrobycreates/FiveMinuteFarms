13d8774d4610c174510e7323b47ecd3518b71ed1
Ashley Roby <ashleyrobycreates@gmail.com>
2024-06-21 23:13:06 -0400

    Added Plant API

---
+++ fiveminutefarms.js
@@ -0,0 +1,10 @@
+var requestOptions = {
+    method: '',
+    redirect: 'follow'
+  };
+  
+  fetch(`https://perenual.com/api/species-list?key=[sk-rfXa66763ec8237056010]`, requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+  
