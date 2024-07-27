2bb8455b052bcea0b6348920b5298a74fba847e1
Ashley Roby <ashleyrobycreates@gmail.com>
2024-07-27 01:39:34 -0400

    JS

--- src/script.js
+++ src/script.js
@@ -33,7 +33,7 @@ export async function redirectToAuthCodeFlow(clientId) {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("redirect_uri", "http://127.0.0.1:5500/");
     params.append("scope", "user-read-private user-read-email");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
