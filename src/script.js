052b03d7e6433b4250f68cb79f59f636f8dbff1e
Ashley Roby <ashleyrobycreates@gmail.com>
2024-07-27 02:14:19 -0400

    Spotify WEB API changes/fixes

--- src/script.js
+++ src/script.js
@@ -1,5 +1,8 @@
-const clientId = "daafaef84f6645a58fa7a92405cf7bbc"; // Replace with your client ID
-const code = undefined;
+const clientId = "daafaef84f6645a58fa7a92405cf7bbc"; 
+// Replace with your clientID const clientId = "your_client_id";?
+const params = new URLSearchParams(window.location.search);
+const code = params.get("code"); ID
+
 
 if (!code) {
     redirectToAuthCodeFlow(clientId);
@@ -58,4 +61,23 @@ async function generateCodeChallenge(codeVerifier) {
         .replace(/\+/g, '-')
         .replace(/\//g, '_')
         .replace(/=+$/, '');
+}
+export async function getAccessToken(clientId, code) {
+    const verifier = localStorage.getItem("verifier");
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("grant_type", "authorization_code");
+    params.append("code", code);
+    params.append("redirect_uri", "http://127.0.0.1:5500/");
+    params.append("code_verifier", verifier);
+
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: params
+    });
+
+    const { access_token } = await result.json();
+    return access_token;
 }
