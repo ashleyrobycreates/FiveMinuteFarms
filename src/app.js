f24355130da87dae00fb7ad1af622fab35f1cc38
Ashley Roby <ashleyrobycreates@gmail.com>
2024-07-30 03:56:00 -0400

    Figuring out how to get access token

---
+++ src/app.js
@@ -0,0 +1,19 @@
+var client_id = 'CLIENT_ID';
+var client_secret = 'CLIENT_SECRET';
+
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    var token = body.access_token;
+  }
+});
