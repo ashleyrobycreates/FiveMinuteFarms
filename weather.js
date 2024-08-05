e748ce2bdc807a8aaa80d9b0d7c6264ac52ae537
Ashley Roby <ashleyrobycreates@gmail.com>
2024-08-05 00:21:58 -0400

    .

--- weather.js
+++ weather.js
@@ -66,10 +66,11 @@ function displayWeather(data) {
         const iconCode = data.weather[0].icon;
         const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@4x.png`;
     
-        const temperatureHTML = '<p>${temperature}°C</p>;
+        const temperatureHTML = '
+            <p>${temperature}°C</p>
         ';
         const weatherHtml = '
-            <p>${cityName}</p>
+            <p>${cityName}</p> 
             <p>${description}</p>
         ';
 
@@ -82,8 +83,30 @@ function displayWeather(data) {
     }
 }
         
-        
-        
+function displayHourlyForecast(hourlyData) {
+
+        const hourlyForecastDiv = document.getElementById('hourly-forecast');
+        const next24Hours = hourlyData.list.slice(0, 8);
+
+        next24Hours.forEach(item => {
+
+            const dateTime = new Date(item.dt * 1000);
+            const hour = dateTime.getHours();
+            const temperature = Math.round(item.main.temp - 273.15);
+            const iconCode = item.weather[0].icon;
+            const iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
+
+            const hourlyItemHtml = '
+            <div class="hourly-item">
+                <span>${hour}:00</span>
+                <img src="${iconUrl}" alt="Hourly Weather Icon">
+                <span>${temperature}°C</span>
+            </div>
+        ';
+        hourlyForecastDiv.innerHTML += hourlyItemHtml;
+
+    });
+      
         
 
         
