fafb0feceff27a5c560d3ad382d9237f92805eb4
Ashley Roby <ashleyrobycreates@gmail.com>
2024-08-05 02:51:39 -0400

    making const

--- weather.js
+++ weather.js
@@ -1,3 +1,17 @@
 const apiKey = APIKEY;
-const 
+const dateObj = new Date();
 
+const getDayName = (dayType, dateVal = dateObj) => 
+    toLocaleDateString('en-US', { weekday: dayType });
+
+function
+
+navigator.geolocation.getCurrentPosition(position) => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const location = '$(latitude),$(longitude)';
+    
+    fetchWeatherData(location);
+}, error => {
+    console.error('Error-getting-location', error);
+})
