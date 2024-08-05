2f5b5000164e328f2de6d788ed6781ce9421cf15
Ashley Roby <ashleyrobycreates@gmail.com>
2024-08-04 23:46:31 -0400

    .

--- weather.js
+++ weather.js
@@ -41,3 +41,61 @@ function getWeather() {
     });
     
 }
+//displayWeather function
+function displayWeather(data) {
+    
+    const tempDivInfo = document.getElementById('temp-info');
+    const weatherInfoDiv = document.getElementById('weather-info');
+    const weatherIcon = document.getElementById('weather-icon');
+    const hourlyForecastDiv = document.getElementById('hourly-forecast');
+
+    //Clear previous content
+    weatherInfoDiv.innerHTML = '';
+    hourlyForecastDiv.innerHTML = '';
+    tempDivInfo.innerHTML = '';
+}
+
+function displayWeather(data) {
+    
+    if (data.cod === '404') {
+       weatherInfoDiv.innerHTML = <p>${data.message}</p>;
+    } else {
+        const cityName = data.name;
+        const temperature = Math.round(data.main.temp - 273.15);
+        const description = data.weather[0].description;
+        const iconCode = data.weather[0].icon;
+        const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@4x.png`;
+    
+        const temperatureHTML = '<p>${temperature}°C</p>;
+        ';
+        const weatherHtml = '
+            <p>${cityName}</p>
+            <p>${description}</p>
+        ';
+
+        tempDivInfo.innerHTML = temperatureHTML;
+        weatherInfoDiv.innerHTML = weatherHtml;
+        weatherIcon.src = iconUrl;
+        weatherIcon.alt = description;
+
+        showImage();
+    }
+}
+        
+        
+        
+        
+
+        
+    
+    
+    
+    
+    
+    }
+}
+
+
+
+
+   
