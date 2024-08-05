3fe08186a65a30a6fae0275aec6da76a9cffe2af
Ashley Roby <ashleyrobycreates@gmail.com>
2024-08-05 03:15:23 -0400

    data

--- weather.js
+++ weather.js
@@ -4,7 +4,16 @@ const dateObj = new Date();
 const getDayName = (dayType, dateVal = dateObj) => 
     toLocaleDateString('en-US', { weekday: dayType });
 
-function
+function fetchWeatherData(location){
+    const apiUrl = 'https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${encodeURIComponent(location)}&days=5';
+
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+
+        console.log(data)
+    })
+}
 
 navigator.geolocation.getCurrentPosition(position) => {
     const latitude = position.coords.latitude;
