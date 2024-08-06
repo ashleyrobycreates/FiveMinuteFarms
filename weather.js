e31666553f95f3e055e245a7ac74f57f516b2cbc
Ashley Roby <ashleyrobycreates@gmail.com>
2024-08-06 19:05:02 -0400

    Rebuilding weather API

---
+++ weather.js
@@ -0,0 +1,38 @@
+const dateObj = new Date();
+
+function fetchWeatherData(location) {
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=6fecc3120ad242a68d363538240608&q=${location}&days=5`;
+
+    fetch (apiUrl)
+    .then(response => {
+        //Make sure the response is OK
+        if (!response.ok) {
+            throw new Error('Network response error' + response.statusText);
+        }
+        //Convert response to JSON
+        return response.json();
+        
+    })
+    .then(data => {
+        //Log JSON data
+        console.log(data);
+        ;
+    })
+    .catch(error => {
+        //handle the error
+        console.error('There has been a problem with your fetch operation:', error);
+    });
+}
+
+fetchWeatherData();
+
+navigator.geolocation.getCurrentPosition(function(position => {
+   const latitude = position.coords.latitude;
+   const longitude = position.coords.longitude;
+   const location = `${latitude}, ${longitude}`;
+   fetchWeatherData(location);
+
+},error => {
+    
+});
+    
