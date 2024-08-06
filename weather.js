ecca46979df96f219e6925bb2b018b3defd9e5c7
Ashley Roby <ashleyrobycreates@gmail.com>
2024-08-06 01:41:01 -0400

    api key issue resolved

--- weather.js
+++ weather.js
@@ -1,4 +1,8 @@
-const apiKey = APIKEY;
+// Load environment variables from .env file
+require('dotenv').config();
+
+// Access the API key from environment variables
+const apiKey = process.env.APIKEY;
 const dateObj = new Date();
 
 const getDayName = (dayType, dateVal = dateObj) => dateVal. 
