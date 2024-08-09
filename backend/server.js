f8742ac044e27e78ce9a8cdbce5ff7f644832115
Ashley Roby <ashleyrobycreates@gmail.com>
2024-08-09 13:48:31 -0400

    .

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config({path: '../.env'});
 
 const app = express();
 const port = process.env.PORT || 3000;
